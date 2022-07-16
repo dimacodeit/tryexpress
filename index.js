c1ba1c52da1aa9ec612a6d3f8cf957fd2c785d66
dmitry <dmitryspiridonov56@gmail.com>
2022-07-16 12:53:33 +0300

    post

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const { json } = require('express');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Ты пидор!')
-})
+  res.send('Ты пидор!');
+});
+
+app.post('/', (req, res) => {
+  res.json(req.body)
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
