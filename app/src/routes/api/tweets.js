9447b035d8858e568c8a3c293b191ddb89fd167a
dmitry <dmitryspiridonov56@gmail.com>
2022-10-29 16:12:11 +0300

    test

--- app/src/routes/api/tweets.js
+++ app/src/routes/api/tweets.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const db = require('@firebase');
+const db = require('../../../../firebase');
 
 const timestamp = require('@utils/timestamp');
 const logger = require('@utils/logger');
